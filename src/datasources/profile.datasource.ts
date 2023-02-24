fb477494a24bbcd497b38b8401df84853069a8ea
Guillermo Mejías <gmejias97@gmail.com>
2023-02-24 09:48:40 +0100

    add deployment

--- src/datasources/profile.datasource.ts
+++ src/datasources/profile.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'profile',
   connector: 'mongodb',
-  url: process.env.DB_URL ?? 'mongodb://localhost:27017/profile?replicaSet=rs0',
+  url: process.env.DB_URL + '?replicaSet=rs0' ?? 'mongodb://localhost:27017/profile?replicaSet=rs0',
   host: process.env.DB_HOST ?? 'localhost',
   port: process.env.MONGO_PORT ?? 27017,
   user: process.env.DB_USER,
